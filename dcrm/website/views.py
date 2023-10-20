30f5034b8184eb1afad78e4d49d2501263c28fb2
Abdelrahman <abdelrahman.ayman8642@gmail.com>
2023-10-20 23:37:33 +0300

    viewing recors adding also deleting

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
+from .models import Record
 
 
 # Create your views here.
 def home(request):
+    records = Record.objects.all()
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
@@ -18,7 +20,7 @@ def home(request):
             messages.error(request, "there was an error")
             return redirect("home")
 
-    return render(request, "home.html", {})
+    return render(request, "home.html", {"records": records})
 
 
 def user_login(request):
@@ -47,3 +49,38 @@ def register_user(request):
         form = SignUpForm()
 
     return render(request, "register.html", {"form": form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        # Look Up Records
+        customer_record = Record.objects.get(id=pk)
+        return render(request, "record.html", {"customer_record": customer_record})
+    else:
+        messages.success(request, "You Must Be Logged In To View That Page...")
+        return redirect("home")
+
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Record Deleted Successfully...")
+        return redirect("home")
+    else:
+        messages.success(request, "You Must Be Logged In To Do That...")
+        return redirect("home")
+
+
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Record Added...")
+                return redirect("home")
+        return render(request, "add_record.html", {"form": form})
+    else:
+        messages.success(request, "You Must Be Logged In...")
+        return redirect("home")
